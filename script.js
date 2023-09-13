44a2a538a1467f6fc8b95d3a88e9f2cc5e0350ac
Sergey Peshkov <zloi.g1rz@gmail.com>
2023-09-13 09:36:36 +0800

    add quoted

--- script.js
+++ script.js
@@ -12,15 +12,16 @@ const download = document.querySelector('#download');
 const downloadText = document.querySelector('#downloadText');
 
 const vcfTemplate = `BEGIN:VCARD
-VERSION:2.1
-ORG;CHARSET=UTF-8:Адикт
-TITLE;CHARSET=UTF-8:Программист
-FN;CHARSET=UTF-8:Пешков Сергей НиколавечиTEST
+VERSION:3.0
+ORG;CHARSET=UTF-8:=D0=90=D0=B4=D0=B8=D0=BA=D1=82
+TITLE;CHARSET=UTF-8:=D0=9F=D1=80=D0=BE=D0=B3=D1=80=D0=B0=D0=BC=D0=BC=D0=B8=D1=81=D1=82
+FN;CHARSET=UTF-8:=D0=9F=D0=B5=D1=88=D0=BA=D0=BE=D0=B2 =D0=A1=D0=B5=D1=80=D0=B3=D0=B5=D0=B9 =
+=D0=9D=D0=B8=D0=BA=D0=BE=D0=BB=D0=B0=D0=B2=D0=B5=D1=87=D0=B8TEST
 EMAIL;type=WORK:ps@adictgroup.ru
 TEL;TYPE=VOICE:89501284824
 END:VCARD`;
 
-const blobContact = new Blob([vcfTemplate], {type: 'text/x-vcard;charset=UTF-8'});
+const blobContact = new Blob([vcfTemplate], {type: 'text/x-vcard'});
 download.href = URL.createObjectURL(blobContact);
 download.download = 'vcard.vcf';
 
